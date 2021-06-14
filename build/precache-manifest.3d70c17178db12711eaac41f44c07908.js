42a4d40553d3042a71123be27a60902f26c67ee0
Sahil <56653026+sahilg50@users.noreply.github.com>
2021-06-14 23:25:14 +0530

    bug fixes

---
+++ build/precache-manifest.3d70c17178db12711eaac41f44c07908.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f8b9523a4faeb2a8fa570914e9ae06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9b6963c66ca1870d57",
+    "url": "/static/css/3.ed673b97.chunk.css"
+  },
+  {
+    "revision": "9002c515cc052332ecc5",
+    "url": "/static/css/4.0110b6a7.chunk.css"
+  },
+  {
+    "revision": "6409efbbdebe70d97e3f",
+    "url": "/static/css/main.35963e0f.chunk.css"
+  },
+  {
+    "revision": "e4e5ce79597aa914d73d",
+    "url": "/static/js/2.d5ce9763.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/static/js/2.d5ce9763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9b6963c66ca1870d57",
+    "url": "/static/js/3.2b9f3812.chunk.js"
+  },
+  {
+    "revision": "9002c515cc052332ecc5",
+    "url": "/static/js/4.e1109f6f.chunk.js"
+  },
+  {
+    "revision": "6409efbbdebe70d97e3f",
+    "url": "/static/js/main.8b7af45b.chunk.js"
+  },
+  {
+    "revision": "40c8058537a585edc9a1",
+    "url": "/static/js/runtime-main.0f29cc6c.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "c7516a7b5f89cf9ffab474f195962649",
+    "url": "/static/media/adventure.c7516a7b.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "6de7832a034236118a400c48f03b1e49",
+    "url": "/static/media/deeplearning.6de7832a.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c66abde19e8e97eb2eba85426509868e",
+    "url": "/static/media/orion.c66abde1.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "2699279eaf7865396dc5b9dfdc74b17c",
+    "url": "/static/media/zany.2699279e.png"
+  }
+]);
